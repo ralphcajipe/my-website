f6be5d5b6edadb5c4505a28fff221f123948b650
Ralph Cajipe <ralphcajipe@gmail.com>
2024-12-22 21:13:55 +0800

    fix: contact form responsiveness

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
             </div>
         </div>
 
-                <!-- Timeline Section -->
+        <!-- Timeline Section -->
         <div class="space-y-4">
             <!-- DOST - ASTI -->
             <div class="flex flex-col md:flex-row gap-4">
@@ -62,67 +62,97 @@
                 <div class="flex-grow border-l-2 border-gray-300 pl-4 pb-4">
                     <div class="flex items-center gap-4 mb-2">
                         <div class="w-12 h-12 flex-shrink-0">
-                            <img src="{{ asset('images/DOST-ASTI-Logo-RGB.png') }}" alt="Company Logo" class="w-full h-full object-contain">
+                            <img src="{{ asset('images/DOST-ASTI-Logo-RGB.png') }}" alt="Company Logo"
+                                class="w-full h-full object-contain">
                         </div>
                         <div>
-                            <p class="text-gray-600 text-xs md:text-sm lg:text-base">I was an Artificial Intelligence Intern for Philippines' <a href="https://asti.dost.gov.ph/" class="text-blue-500 underline hover:underline">Department of Science and Technology - Advanced Science and Technology Institute</a>, where I contributed to the Research and Development Division's <a href="https://youtu.be/nD-Ttj9jy5c?si=9KarBaHhgjyzvm3M" class="text-blue-500 underline hover:underline">Project ROAMER (Robot for Optimized and Autonomous Mission-Enhancement Response)</a>. This involved developing a smart robot capable of detecting banana plant diseases. This includes in-house data labeling, neural network training, the science of making it work, and deployment in production running on our AI supercomputer. Today, it aims to advance agricultural diagnostics, supporting farmers with early disease detection for healthier crops.</p>
+                            <p class="text-gray-600 text-xs md:text-sm lg:text-base">I was an Artificial Intelligence
+                                Intern for Philippines' <a href="https://asti.dost.gov.ph/"
+                                    class="text-blue-500 underline hover:underline">Department of Science and Technology
+                                    - Advanced Science and Technology Institute</a>, where I contributed to the Research
+                                and Development Division's <a href="https://youtu.be/nD-Ttj9jy5c?si=9KarBaHhgjyzvm3M"
+                                    class="text-blue-500 underline hover:underline">Project ROAMER (Robot for Optimized
+                                    and Autonomous Mission-Enhancement Response)</a>. This involved developing a smart
+                                robot capable of detecting banana plant diseases. This includes in-house data labeling,
+                                neural network training, the science of making it work, and deployment in production
+                                running on our AI supercomputer. Today, it aims to advance agricultural diagnostics,
+                                supporting farmers with early disease detection for healthier crops.</p>
                         </div>
                     </div>
                     <div class="flex flex-col md:flex-row gap-4 mt-4">
                         <!-- Left Image Container -->
                         <div class="w-full md:w-1/2">
-                            <img src="{{ asset('images/roamer.png') }}" alt="Left Image" class="w-full h-full object-cover">
+                            <img src="{{ asset('images/roamer.png') }}" alt="Left Image"
+                                class="w-full h-full object-cover">
                         </div>
                         <!-- Right Image Container (Collage) -->
                         <div class="w-full md:w-1/2 grid grid-cols-2 gap-2">
-                            <img src="{{ asset('images/banana-disease.png') }}" alt="Collage Image 1" class="w-full h-48 object-cover">
+                            <img src="{{ asset('images/banana-disease.png') }}" alt="Collage Image 1"
+                                class="w-full h-48 object-cover">
                             <img src="{{ asset('images/ppt.png') }}" alt="Collage Image 2" class="w-full object-cover">
-                            <img src="{{ asset('images/yolo.png') }}" alt="Collage Image 3" class="w-full object-cover col-span-2">
+                            <img src="{{ asset('images/yolo.png') }}" alt="Collage Image 3"
+                                class="w-full object-cover col-span-2">
                         </div>
                     </div>
                 </div>
             </div>
-        
+
             <!-- Invisible Hand Inc. -->
             <div class="flex flex-col md:flex-row gap-4">
                 <div class="flex-shrink-0 w-24 text-left md:text-right text-gray-500"></div>
                 <div class="flex-grow border-l-2 border-gray-300 pl-4 pb-4">
                     <div class="flex items-center gap-4 mb-2">
                         <div class="w-12 h-12 flex-shrink-0">
-                            <img src="{{ asset('images/invisible.png') }}" alt="Company Logo" class="w-full h-full object-contain">
+                            <img src="{{ asset('images/invisible.png') }}" alt="Company Logo"
+                                class="w-full h-full object-contain">
                         </div>
                         <div>
-                            <p class="text-gray-600 text-xs md:text-sm lg:text-base">I was a Web Developer Intern at <a href="https://www.facebook.com/invisiblehandinc" class="text-blue-500 underline hover:underline">Invisible Hand Inc.</a>, where I developed and maintained a web application called <a href="https://talacargosolutions.com/one-crystal" class="text-blue-500 underline hover:underline">One Crystal Logistics</a>. Today, it provides trucking and customs brokerages service that caters to the need of handful importers in cleaning air freight and sea freight shipments from the Bureau of Customs.</p>
+                            <p class="text-gray-600 text-xs md:text-sm lg:text-base">I was a Web Developer Intern at <a
+                                    href="https://www.facebook.com/invisiblehandinc"
+                                    class="text-blue-500 underline hover:underline">Invisible Hand Inc.</a>, where I
+                                developed and maintained a web application called <a
+                                    href="https://talacargosolutions.com/one-crystal"
+                                    class="text-blue-500 underline hover:underline">One Crystal Logistics</a>. Today, it
+                                provides trucking and customs brokerages service that caters to the need of handful
+                                importers in cleaning air freight and sea freight shipments from the Bureau of Customs.
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
-        
+
             <!-- FEU Tech timeline -->
             <div class="flex flex-col md:flex-row gap-4">
                 <div class="flex-shrink-0 w-24 text-left md:text-right text-gray-500">2019-2024</div>
                 <div class="flex-grow border-l-2 border-gray-300 pl-4 pb-4">
                     <div class="flex items-center gap-4 mb-2">
                         <div class="w-12 h-12 flex-shrink-0">
-                            <img src="{{ asset('images/FEU_Tech_official_seal.png') }}" alt="Company Logo" class="w-full h-full object-contain">
+                            <img src="{{ asset('images/FEU_Tech_official_seal.png') }}" alt="Company Logo"
+                                class="w-full h-full object-contain">
                         </div>
                         <div>
-                            <p class="text-gray-600 text-xs md:text-sm lg:text-base">BSc Computer Science at FEU Institute of Technology with a major in Software Engineering. I transferred here to continue my degree and this is where I specialized on AI/machine learning and web applications.</p>
+                            <p class="text-gray-600 text-xs md:text-sm lg:text-base">BSc Computer Science at FEU
+                                Institute of Technology with a major in Software Engineering. I transferred here to
+                                continue my degree and this is where I specialized on AI/machine learning and web
+                                applications.</p>
                         </div>
                     </div>
                 </div>
             </div>
-        
+
             <!-- Wesleyan University-Philippines timeline -->
             <div class="flex flex-col md:flex-row gap-4">
                 <div class="flex-shrink-0 w-24 text-left md:text-right text-gray-500">2018-2019</div>
                 <div class="flex-grow border-l-2 border-gray-300 pl-4 pb-4">
                     <div class="flex items-center gap-4 mb-2">
                         <div class="w-12 h-12 flex-shrink-0">
-                            <img src="{{ asset('images/wup.png') }}" alt="Company Logo" class="w-full h-full object-contain">
+                            <img src="{{ asset('images/wup.png') }}" alt="Company Logo"
+                                class="w-full h-full object-contain">
                         </div>
                         <div>
-                            <p class="text-gray-600 text-xs md:text-sm lg:text-base">BSc Computer Science at Wesleyan University-Philippines. I spent one year studying here, this is where I first got into software development such as web applications and algorithms.</p>
+                            <p class="text-gray-600 text-xs md:text-sm lg:text-base">BSc Computer Science at Wesleyan
+                                University-Philippines. I spent one year studying here, this is where I first got into
+                                software development such as web applications and algorithms.</p>
                         </div>
                     </div>
                 </div>
@@ -266,41 +296,41 @@
         </div>
 
         <!-- Featured Writing Section -->
-        <div class="bg-white rounded-lg shadow-lg p-8 mt-8">
-            <h2 class="text-2xl font-bold text-gray-900 mb-4">featured writing</h2>
-            <p class="text-gray-600 mb-4">Here are some of my short blogs on Medium:</p>
+        <div class="bg-white rounded-lg shadow-lg p-4 md:p-8 mt-8">
+            <h2 class="text-xl md:text-2xl font-bold text-gray-900 mb-4">featured writing</h2>
+            <p class="text-gray-600 text-sm md:text-base mb-4">Here are some of my short blogs on Medium:</p>
             <div class="space-y-2">
                 <div>
-                    <p class="text-gray-600">Feb 2022 - <a
+                    <p class="text-gray-600 text-sm md:text-base">Feb 2022 - <a
                             href="https://medium.com/@computeralph/the-age-of-a-i-a6766679d453"
-                            class="text-blue-500 hover:underline">The Age of A.I.</a></p>
+                            class="text-blue-500 underline hover:underline">The Age of A.I.</a></p>
                 </div>
                 <div>
-                    <p class="text-gray-600">Feb 2022 - <a
+                    <p class="text-gray-600 text-sm md:text-base">Feb 2022 - <a
                             href="https://medium.com/@computeralph/path-to-a-free-self-taught-education-in-computer-science-9ee6a0b79fb1"
-                            class="text-blue-500 hover:underline">Path to a free self-taught education in Computer
-                            Science</a></p>
+                            class="text-blue-500 underline hover:underline">Path to a free self-taught education in
+                            Computer Science</a></p>
                 </div>
                 <!-- Add more writing items as needed -->
             </div>
         </div>
 
         <!-- Research Section -->
-        <div class="bg-gray-200 rounded-lg shadow-lg p-8 mt-8">
-            <h2 class="text-2xl font-bold text-gray-900 mb-4">research</h2>
+        <div class="bg-gray-200 rounded-lg shadow-lg p-4 md:p-8 mt-8">
+            <h2 class="text-xl md:text-2xl font-bold text-gray-900 mb-4">research</h2>
             <div class="space-y-4">
                 <!-- Research Item -->
                 <div class="flex flex-col md:flex-row gap-4">
                     <div class="flex-grow border-l-4 border-gray-400 pl-4">
                         <div class="mb-2">
-                            <p class="text-lg font-semibold">
+                            <p class="text-lg font-semibold text-sm md:text-base">
                                 <a href="https://www.researchgate.net/publication/379037487_Video_Content_Analysis_through_Computer_Vision_and_Natural_Language_Processing"
-                                    class="text-blue-500 hover:underline">Video Content Analysis through Computer
-                                    Vision and Natural Language Processing</a>
+                                    class="text-blue-500 underline hover:underline">Video Content Analysis through
+                                    Computer Vision and Natural Language Processing</a>
                                 <span class="text-green-500">2023</span>
                             </p>
-                            <p class="text-red-700">Ralph Cajipe, Charles Santos, Kyle Español, Sally Segundo Jr., Kyle
-                                Baltazar</p>
+                            <p class="text-red-700 text-sm md:text-base">Ralph Cajipe, Charles Santos, Kyle Español,
+                                Sally Segundo Jr., Kyle Baltazar</p>
                         </div>
                     </div>
                 </div>
@@ -309,22 +339,22 @@
         </div>
 
         <!-- Contact Form Section -->
-        <div class="bg-white rounded-lg shadow-lg p-8 mt-8">
-            <h2 class="text-2xl font-bold text-gray-900 mb-4">message me</h2>
+        <div class="bg-white rounded-lg shadow-lg p-4 md:p-8 mt-8">
+            <h2 class="text-xl md:text-2xl font-bold text-gray-900 mb-4">message me</h2>
             <form action="{{ route('contact.store') }}" method="POST">
                 @csrf
                 <div class="mb-4">
-                    <label for="name" class="block text-gray-700">Name</label>
+                    <label for="name" class="block text-gray-700 text-sm md:text-base">Name</label>
                     <input type="text" id="name" name="name" class="w-full px-3 py-2 border rounded-lg"
                         required>
                 </div>
                 <div class="mb-4">
-                    <label for="email" class="block text-gray-700">Email</label>
+                    <label for="email" class="block text-gray-700 text-sm md:text-base">Email</label>
                     <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded-lg"
                         required>
                 </div>
                 <div class="mb-4">
-                    <label for="message" class="block text-gray-700">Message</label>
+                    <label for="message" class="block text-gray-700 text-sm md:text-base">Message</label>
                     <textarea id="message" name="message" class="w-full px-3 py-2 border rounded-lg" rows="5" required></textarea>
                 </div>
                 <div>
